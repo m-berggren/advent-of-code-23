3a4c3f5925f6836ba994d86605295d845e0c156a
marcusberggren <berggren.me@outlook.com>
2023-12-05 23:44:58 +0100

    commit 1st task

---
+++ dec-1/script3.py
@@ -0,0 +1,21 @@
+import sys
+
+sum_of_calibrations = 0
+
+with open(sys.argv[1], 'r') as file:
+
+    for line in file:
+        find_first = True
+        first_char = ""
+        last_char = ""
+
+        for char in line:
+            if char.isdigit():
+                if find_first:
+                    first_char = char
+                    last_char = char
+                    find_first = False
+                else:
+                    last_char = char
+        concat_chars = first_char + last_char
+        sum_of_calibrations += int(concat_chars)
